1ac7349fe89cfe3e53b8aa143dea935de01318ab
nickmwangemi <nickmwangemi@gmail.com>
2021-01-06 22:23:38 +0300

    Update contact.html to contact-me.html

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const http = require('http')
 const fs = require('fs')
 const indexPage = fs.readFileSync('index.html')
 const aboutPage = fs.readFileSync('about.html')
-const contactPage = fs.readFileSync('contact.html')
+const contactPage = fs.readFileSync('contact-me.html')
 const notFoundPage = fs.readFileSync('notFound.html')
 
 // index, about, contact-me and 404.
 const server = http.createServer((req, res) => {
 	if (req.url === '/about') res.end(aboutPage)
-	else if (req.url === '/contact') res.end(contactPage)
+	else if (req.url === '/contact-me') res.end(contactPage)
 	else if (req.url === '/') res.end(indexPage)
 	else {
 		res.writeHead(404)
